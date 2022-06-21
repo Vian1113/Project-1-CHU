1fb758ebba41ef34186514a99c2fa06bc15c14cc
Vivian <vivianhuang197@gmail.com>
2022-06-21 23:27:11 +0800

    修改首頁常見問題效果

--- all.js
+++ all.js
@@ -1,33 +1,73 @@
 
 
-$(document).ready(function (){
-    $(".faqcontent").click(function(){
-        var x = $(this).siblings("p")
-        if(x.css("display") == "none"){
-            x.slideDown();
-        }else{
-            x.slideUp();
-        };
+$(document).ready(function () {
 
+    //漢堡選單
+    $('.hambar').click(function (event) {
+        event.preventDefault();
+        $('.navmobile').slideToggle(600);
     });
 
-    $(window).scroll(function(){
-        
-        var scrolltop =$(window).scrollTop();
+    $('.navmobile li a').click(function (event) {
+        event.preventDefault();
+        //收起來
+        $('.navmobile').slideUp(600);
+    });
 
-        if(scrolltop >=500){
+
+
+    //常見問題
+    
+    $(".faqcontent").click(function (e) {
+        e.preventDefault();
+        
+        $(this).find("h5").toggleClass("active");
+        var y=true;
+        var x = $(this).siblings("p");
+        if (x.css("display") == "none") {
+            x.slideDown();
+            $(this).find("img").attr("src","img/dash.svg")
+        } else {
+            x.slideUp();
+            $(this).find("img").attr("src","img/plus.svg")
+        };
+        y =!y;
+        
+    });
+
+    // var y = true;
+    //  $(".faqcontent").click(function (e) {
+    //      e.preventDefault();
+    //     if(y){
+    //         $(this).find("img").attr("src","img/dash.svg")
+    //     }else{
+    //         $(this).find("img").attr("src","img/plus.svg")
+    //     }
+
+    //      y =!y;
+        
+
+    //  });
+
+
+    //懸浮常用icon與pagetop
+    $(window).scroll(function () {
+
+        var scrolltop = $(window).scrollTop();
+
+        if (scrolltop >= 500) {
             $("#pagetop").show();
             $("#fixedicon").show();
             $("#socialicon").show();
-        }else{
+        } else {
             $("#pagetop").hide();
             $("#fixedicon").hide();
             $("#socialicon").hide();
         }
-       
 
 
-   });
+
+    });
 
 
 
